28235089ab81339bb0b5f05d665b7e1d951462b7
jeffy555 <jeffersonimmanuel5@gmail.com>
2025-11-16 14:38:49 +0530

    feat: add Azure resources including storage account and app service plan

--- main.tf
+++ main.tf
@@ -10,3 +10,38 @@ resource "azurerm_storage_account" "test_sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_container_app_environment" "my_container_app_env" {
+  name                = "my-container-app-env"
+  resource_group_name = azurerm_resource_group.test_rg.name
+  location            = azurerm_resource_group.test_rg.location
+  app_logs_configuration {
+    log_analytics {
+      workspace_id = azurerm_log_analytics_workspace.my_workspace.id
+      workspace_key = azurerm_log_analytics_workspace.my_workspace.primary_shared_access_key
+    }
+  }
+}
+
+resource "azurerm_app_service_plan" "my_app_service_plan" {
+  name                = "my-app-service-plan"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+  sku {
+    tier     = "PremiumV2"
+    size     = "P1v2"
+    capacity = 1
+  }
+  kind = "Linux"
+}
+
+resource "azurerm_app_service" "my_app_service" {
+  name                = "my-linux-app-service"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+  app_service_plan_id = azurerm_app_service_plan.my_app_service_plan.id
+  kind               = "Linux"
+  site_config {
+    linux_fx_version = "DOCKER|mydockerhub/myapp:latest"
+  }
+}
