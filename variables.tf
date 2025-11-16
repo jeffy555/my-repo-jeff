63aaca70068648e8eb5c5cdaa714cc1fce6a9ede
jeffy555 <jeffersonimmanuel5@gmail.com>
2025-11-16 14:38:51 +0530

    feat: add Azure resources including storage account and app service plan

--- variables.tf
+++ variables.tf
@@ -3,3 +3,33 @@ variable "location" {
   type        = string
   default     = "East US"
 }
+
+variable "container_app_env_name" {
+  description = "Name of the Container App Environment"
+  type        = string
+  default     = "my-container-app-env"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "my-app-service-plan"
+}
+
+variable "app_service_name" {
+  description = "Name of the App Service"
+  type        = string
+  default     = "my-linux-app-service"
+}
+
+variable "app_service_plan_tier" {
+  description = "Pricing tier for the App Service Plan"
+  type        = string
+  default     = "PremiumV2"
+}
+
+variable "app_service_plan_size" {
+  description = "Size for the App Service Plan"
+  type        = string
+  default     = "P1v2"
+}
