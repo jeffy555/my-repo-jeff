27c497b4b32ce44f44163074641b0a55e17106a3
jeffy555 <jeffersonimmanuel5@gmail.com>
2025-11-16 14:19:20 +0530

    feat: add resource group and storage account in Azure configuration

--- main.tf
+++ main.tf
@@ -1,34 +1,12 @@
-resource "azurerm_resource_group" "my_resource_group" {
-  name     = "my-resource-group"
-  location = var.location
+resource "azurerm_resource_group" "test_rg" {
+  name     = "test-rg-commit"
+  location = "East US"
 }
 
-resource "azurerm_container_registry" "my_acr" {
-  name                = "mycontainerregistry"
-  resource_group_name = azurerm_resource_group.my_resource_group.name
-  location            = azurerm_resource_group.my_resource_group.location
-  sku                 = "Basic"
-  admin_enabled       = false  # Admin account disabled for security compliance
-}
-
-resource "azurerm_container_app_environment" "my_container_app_env" {
-  name                = "my-container-app-env"
-  resource_group_name = azurerm_resource_group.my_resource_group.name
-  location            = azurerm_resource_group.my_resource_group.location
-  ingress {
-    external_enabled = true
-    target_port     = 80
-  }
-}
-
-resource "azurerm_container_app" "my_container_app" {
-  name                = "my-container-app"
-  resource_group_name = azurerm_resource_group.my_resource_group.name
-  location            = azurerm_resource_group.my_resource_group.location
-  container {
-    image = "mycontainerregistry.azurecr.io/myapp:latest"
-    cpu   = 0.5
-    memory = 1.0
-  }
-  environment_id = azurerm_container_app_environment.my_container_app_env.id
+resource "azurerm_storage_account" "test_sa" {
+  name                     = "teststoragecommit001"
+  resource_group_name      = azurerm_resource_group.test_rg.name
+  location                 = azurerm_resource_group.test_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
