ae68a8c0ce56aff9a71de8ff7f532e018e13b642
jeffy555 <jeffersonimmanuel5@gmail.com>
2025-11-16 17:36:32 +0530

    feat: add Azure resources for function app deployment and configuration

--- main.tf
+++ main.tf
@@ -1,47 +1,44 @@
-resource "azurerm_resource_group" "test_rg" {
-  name     = "test-rg-commit"
-  location = "East US"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_storage_account" "test_sa" {
-  name                     = "teststoragecommit001"
-  resource_group_name      = azurerm_resource_group.test_rg.name
-  location                 = azurerm_resource_group.test_rg.location
-  account_tier             = "Standard"
+resource "azurerm_resource_group" "function_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "function_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.function_rg.name
+  location                 = azurerm_resource_group.function_rg.location
+  account_tier            = "Standard"
   account_replication_type = "LRS"
+  min_tls_version         = "TLS1_2" // Ensure the latest version of TLS encryption
+  allow_blob_public_access = false    // Disallow public access
 }
 
-resource "azurerm_container_app_environment" "my_container_app_env" {
-  name                = "my-container-app-env"
-  resource_group_name = azurerm_resource_group.test_rg.name
-  location            = azurerm_resource_group.test_rg.location
-  app_logs_configuration {
-    log_analytics {
-      workspace_id = azurerm_log_analytics_workspace.my_workspace.id
-      workspace_key = azurerm_log_analytics_workspace.my_workspace.primary_shared_access_key
-    }
-  }
-}
-
-resource "azurerm_app_service_plan" "my_app_service_plan" {
-  name                = "my-app-service-plan"
-  location            = azurerm_resource_group.test_rg.location
-  resource_group_name = azurerm_resource_group.test_rg.name
+resource "azurerm_app_service_plan" "function_plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.function_rg.location
+  resource_group_name = azurerm_resource_group.function_rg.name
   sku {
-    tier     = "PremiumV2"
-    size     = "P1v2"
-    capacity = 1
+    tier     = "Dynamic"
+    size     = "Y1"
   }
-  kind = "Linux"
 }
 
-resource "azurerm_app_service" "my_app_service" {
-  name                = "my-linux-app-service"
-  location            = azurerm_resource_group.test_rg.location
-  resource_group_name = azurerm_resource_group.test_rg.name
-  app_service_plan_id = azurerm_app_service_plan.my_app_service_plan.id
-  kind               = "Linux"
-  site_config {
-    linux_fx_version = "DOCKER|mydockerhub/myapp:latest"
+resource "azurerm_function_app" "function_app" {
+  name                       = var.function_app_name
+  location                   = azurerm_resource_group.function_rg.location
+  resource_group_name        = azurerm_resource_group.function_rg.name
+  storage_account_name       = azurerm_storage_account.function_storage.name
+  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
+  version                    = "~3"
+  os_type                    = "linux"
+  runtime_stack              = var.runtime_stack
+
+  app_settings = {
+    "AzureWebJobsStorage" = azurerm_storage_account.function_storage.primary_connection_string
+    "FUNCTIONS_WORKER_RUNTIME" = var.functions_worker_runtime
   }
 }
