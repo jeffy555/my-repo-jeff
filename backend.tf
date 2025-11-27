2c5bb3d4b3551c7ba3b03e103f4485864e8be4c6
jeffy555 <jeffersonimmanuel5@gmail.com>
2025-11-28 03:11:01 +0530

    feat: add Azure resources for function app and container registry setup

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstate"
-    container_name       = "terraformstate"
+    storage_account_name = "tfstate79556317"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
   }
